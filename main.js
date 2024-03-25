33f2303a76759dae627c4eea9cdc0ddbd03cdf5b
moxun <1786471868@qq.com>
2024-03-25 17:13:59 +0800

    feat: vdom动态生成 dom渲染写死

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
 
-const text = {
-  type: 'text',
-  props: {
-    nodeValue: 'Hello World',
-    children: []
-  },
-}
-const app = {
-  type: 'div',
-  props: {
-    id: 'app',
-    children: [
-      text
-    ]
-  },
+
+/**
+ * 创建文本节点
+ * @param {*} textValue 
+*/
+const createTextNode = (textValue) => {
+  return {
+    type: 'TEXT',
+    props: {
+      nodeValue: textValue,
+      children: []
+    },
+  }
 }
 
-const dom = document.createElement(app.type);
-dom.id = app.props.id;
+/**
+ * 创建元素节点
+ * @param {*} type 节点类型
+ * @param {*} props 元素参数
+ * @param  {...any} children 子元素
+ *
+*/
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children
+    }
+  }
+}
+
+const textVDom = createTextNode('Hello World');
+const App = createElement('div', {id: 'app'}, textVDom)
+
+const dom = document.createElement(App.type);
+
 const textDom = document.createTextNode('');
 
-textDom.nodeValue = text.props.nodeValue;
+textDom.nodeValue = textVDom.props.nodeValue;
 
 dom.append(textDom);
 
